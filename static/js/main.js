0ace365f57fd99df0e9b03c68af6fe89097110f1
João Magalhães <joamag@gmail.com>
2020-10-03 17:53:51 +0100

    Added support for beautiful reports

--- static/js/main.js
+++ static/js/main.js
@@ -54,6 +54,7 @@ jQuery(document).ready(function() {
     const keyboardContainer = jQuery(".keyboard-container");
     const emojisContainer = jQuery(".emojis-container");
     const viewportContainer = jQuery(".viewer-container");
+    const formConsole = jQuery(".form-console");
     const inputViewport = jQuery(".input-viewport");
     const signature = jQuery(".signature");
 
@@ -209,6 +210,8 @@ jQuery(document).ready(function() {
 
     keyboardContainer.bind("key", keyHandler);
     emojisContainer.bind("key", keyHandler);
+
+    formConsole.formconsole();
 });
 
 (function(jQuery) {
@@ -270,6 +273,33 @@ jQuery(document).ready(function() {
                 context.removeClass("lowercase");
             }
         };
+
+        return this;
+    };
+})(jQuery);
+
+(function(jQuery) {
+    jQuery.fn.formconsole = function() {
+        const elements = jQuery(this);
+
+        elements.each(function() {
+            const context = jQuery(this);
+            const button = jQuery(".button", context);
+            const command = jQuery(".input[name=command]", context);
+
+            button.click(function() {
+                const commandValue = command.val();
+                try {
+                    // eslint-disable-next-line no-eval
+                    const result = eval(commandValue);
+                    if (result) alert(result);
+                    else alert("executed");
+                } catch (err) {
+                    alert(err);
+                }
+                command.val("");
+            });
+        });
 
         return this;
     };
