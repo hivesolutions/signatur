18766024ea290c3ee30a5e5456bcf899a2ce3a8e
João Magalhães <joamag@gmail.com>
2020-12-28 00:15:25 +0000

    test: made text parsing more resilient

--- test/lib/util/base.js
+++ test/lib/util/base.js
@@ -2,6 +2,42 @@ const assert = require("assert");
 const lib = require("../../../lib");
 
 describe("Base", function() {
+    describe("#deserializeText()", function() {
+        it("should deserialize simple text", () => {
+            let result;
+
+            result = lib.deserializeText("arial:a|times:b");
+            assert.deepStrictEqual(result, [
+                ["arial", "a"],
+                ["times", "b"]
+            ]);
+
+            result = lib.deserializeText("arial:a|times:bc");
+            assert.deepStrictEqual(result, [
+                ["arial", "a"],
+                ["times", "bc"]
+            ]);
+        });
+
+        it("should deserialize complex text", () => {
+            const result = lib.deserializeText("arial:a|times:::|emoji:ad");
+            assert.deepStrictEqual(result, [
+                ["arial", "a"],
+                ["times", "::"],
+                ["emoji", "ad"]
+            ]);
+        });
+
+        it("should deserialize erroneous text", () => {
+            const result = lib.deserializeText("arial:a|times|emoji:ad");
+            assert.deepStrictEqual(result, [
+                ["arial", "a"],
+                ["times", null],
+                ["emoji", "ad"]
+            ]);
+        });
+    });
+
     describe("#fontText()", function() {
         it("should return proper most relevant fonts", () => {
             let result;
